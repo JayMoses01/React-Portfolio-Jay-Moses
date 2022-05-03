ebc72378b66c2b31fcd949f0400f09f52e5539f5
JayMoses01 <jayrmoses@gmail.com>
2022-05-02 21:03:33 -0700

    Projects are now a component

--- src/Project.js
+++ src/Project.js
@@ -3,91 +3,73 @@ import SlitherIn from './public/slither-in.png'
 import NationalParksSearch from './public/national-park-search-main.png'
 import EmployeeManagementSystem from './public/employee-tracker-01.png'
 import WeatherDashboard from './public/weather-dashboard.png'
+// import projects from './projects'
+
+
+const projects = [
+    {
+        id: 1,
+        name: "'Sliher-In', A Reptile Database",
+        deployedLink: "https://slither-in.herokuapp.com/",
+        gitHubLink: "https://github.com/niccolosaurus/slither-in",
+        image: SlitherIn
+    },
+    {
+        id: 2,
+        name: "National Parks Search",
+        deployedLink: "https://seyaryu.github.io/National-Park-Project/",
+        gitHubLink: "https://github.com/JayMoses01/National-Park-Project",
+        image: NationalParksSearch
+    },
+    {
+        id: 3,
+        name: "Employee Management System",
+        deployedLink: "https://drive.google.com/file/d/10z2v4_x1kqpTTmM7qFNruR_o8t9npst4/view?usp=sharing",
+        gitHubLink: "https://github.com/JayMoses01/hw-12-employee-tracker",
+        image: EmployeeManagementSystem
+    },
+    {
+        id: 4,
+        name: "Weather Dashboard",
+        deployedLink: "https://jaymoses01.github.io/hw-06-weather-dashboard/",
+        gitHubLink: "https://github.com/JayMoses01/hw-06-weather-dashboard",
+        image: WeatherDashboard
+    }
+]
+
 
 
 export default function Project() {
     return (
         <section className="section box ml-0 has-background-grey-lighter pl-0">
 
-        <section className="section ml-0 pt-1">
-            <div className="container ml-0">
-                <div className="columns ml-0">
-                    <div className="column is-2 ml-0">
-                        <h2 className="has-text-weight-bold is-size-4" id="work">Portfolio</h2>
-                    </div>
-
-                    <div class="column is-10 ml-0 box">
-                        <h3 className="pb-2 has-text-weight-bold">"Slither-In", A Reptile Database</h3>
-                        <div className="columns">
-                            <a className="column is-4" href="https://slither-in.herokuapp.com/">Link to Deployed Application</a>
-                            <a className="column is-4" href="https://github.com/niccolosaurus/slither-in">Link to GitHub Repository</a>
+            <section className="section ml-0 pt-1">
+                <div className="container ml-0">
+                    <div className="columns ml-0">
+                        <div className="column is-2 ml-0">
+                            <h2 className="has-text-weight-bold is-size-4" id="work">Portfolio</h2>
                         </div>
-                        <a href="https://slither-in.herokuapp.com/">
-                        <img src={SlitherIn} alt="Slither-In"/></a>
-                    </div>
 
-                </div>
-            </div>
-        </section>
-    
-        <section className="section ml-0">
-            <div className="container ml-0">
-                <div className="columns ml-0">
-                    <div className="column is-2 ml-0">
-                    </div>
-                    <div className="column is-10 ml-0 box">
-                        <h3 className="pb-2 has-text-weight-bold">National Parks Search</h3>
-                        <div className="columns">
-                            <a className="column is-4" href="https://seyaryu.github.io/National-Park-Project/">Link to Deployed Application</a>
-                            <a className="column is-4" href="https://github.com/JayMoses01/National-Park-Project">Link to GitHub Repository</a>
+                        <div class="column is-10 ml-0 box">
+                            {projects.map(item => (
+                                <div>
+                                    <h3 className="pb-2 has-text-weight-bold" key={item.id}>{item.name}</h3>
+                                    <div className="columns">
+                                        <a className="column is-4" href={item.deployedLink}>Link to Deployed Application</a>
+                                        <a className="column is-4" href={item.gitHubLink}>Link to GitHub Repository</a>
+                                    </div>
+                                    <a href={item.deployedLink}>
+                                        <img src={item.image} alt={item.name} /></a>
+                                </div>
+                            ))}
+
                         </div>
-                        <a href="https://seyaryu.github.io/National-Park-Project/">
-                        <img src={NationalParksSearch} alt="National Parks Search"/></a>
+
+
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <section className="section ml-0">
-            <div className="container ml-0">
-                <div className="columns ml-0">
-                    <div className="column is-2 ml-0">
-                    </div>
-                    <div className="column is-10 ml-0 box">
-                        <h3 className="pb-2 has-text-weight-bold">Employee Management System</h3>
-                        <div className="columns">
-                            <a className="column is-4" href="https://drive.google.com/file/d/10z2v4_x1kqpTTmM7qFNruR_o8t9npst4/view?usp=sharing">Link to Walkthrough Demo</a>
-                            <a className="column is-4" href="https://github.com/JayMoses01/hw-12-employee-tracker">Link to GitHub Repository</a>
-                        </div>
-                        <div className="is-flex is-justify-content-center">
-                            <a href="https://drive.google.com/file/d/10z2v4_x1kqpTTmM7qFNruR_o8t9npst4/view?usp=sharing">
-                                <img src={EmployeeManagementSystem}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </section>
-
-        <section className="section ml-0">
-            <div className="container ml-0">
-                <div className="columns ml-0">
-                    <div className="column is-2 ml-0">
-                    </div>
-                    <div className="column is-10 ml-0 box">
-                        <h3 className="pb-2 has-text-weight-bold">Weather Dashboard</h3>
-                        <div className="columns">
-                            <a className="column is-4" href="https://jaymoses01.github.io/hw-06-weather-dashboard/">Link to Deployed Application</a>
-                            <a className="column is-4" href="https://github.com/JayMoses01/hw-06-weather-dashboard">Link to GitHub Repository</a>
-                        </div>
-                        <a href="https://jaymoses01.github.io/hw-06-weather-dashboard/">
-                            <img src={WeatherDashboard} alt="Weather Dashboard"/>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-        
-    </section>
     );
 }
