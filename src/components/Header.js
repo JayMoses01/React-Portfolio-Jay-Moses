f189d96fd5bf786d0574582961433e927885fc47
JayMoses01 <jayrmoses@gmail.com>
2022-05-15 12:53:36 -0700

    Adding navbar burger toggle functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Header({ currentPage, handlePageChange} ) {
+
+    // I got help with the Bulma navbar burger toggle from: https://dev.to/eclecticcoding/bulma-navbar-toogle-with-react-hooks-18ek.
+    const [isActive, setisActive] = useState(false);
 
     return (
         <nav className="navbar has-background-info-dark">
         <div className="navbar-brand">
             <h1 className="is-size-2 has-text-white has-text-weight-bold">Jay Moses</h1>
-            <a className="navbar-burger" id="burger">
+            <a onClick={() => {setisActive(!isActive)}} role="button" className={`navbar-burger id="burger" ${isActive ? "is-active" : ''}`}>
                 <span></span>
                 <span></span>
                 <span></span>
             </a>
         </div>
 
-        <div className="navbar-menu" id="nav-links">
+        <div className={`navbar-menu id="nav-links" ${isActive ? 'is-active' : ''}`}>
             <div className="navbar-end">
                 
                 <a 
